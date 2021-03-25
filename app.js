c54f539a2f7270dd1950a0aa6762282642f4d217
Ryoichi <>
2021-03-25 23:25:14 +0900

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const app = () => {
+  const song = document.querySelector('song');
+  const play = document.querySelector('play');
+  const song = document.querySelector('s');
+  const song = document.querySelector('song');
+}
+
+app();
